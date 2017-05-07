b071b9e5764ca0cac7516df4f93a4a2bccdb8748
caentrena <caentrena@gmail.com>
2017-05-07 21:46:45 +0200

    typo changed

--- select-room.php
+++ select-room.php
@@ -10,7 +10,7 @@
             <div class="rooms-cards-container">
                 <div class="room-card-container">
                     <div class="photo-container" style="background-image: url('assets/image/habitacion-aguadulce-slider1.jpg');">
-                        <div class="discount"><p>100€</p></div>
+                        <div class="discount"><p>Desde 100€</p></div>
                     </div>
                     <div class="title"><p>Habitación Aguadulce</p></div>
                     <div class="description">Muy lejos, más allá de las montañas de palabras, alejados de los países.</div>
@@ -22,7 +22,7 @@
                 </div>
                 <div class="room-card-container">
                     <div class="photo-container" style="background-image: url('assets/image/habitacion-roquetas-slider1.jpg');">
-                        <div class="discount"><p>50€</p></div>
+                        <div class="discount"><p>Desde 50€</p></div>
                     </div>
                     <div class="title">Habitación Roquetas</div>
                     <div class="description">Muy lejos, más allá de las montañas de palabras, alejados de los países.</div>
@@ -32,7 +32,7 @@
                     </div>                </div>
                 <div class="room-card-container">
                     <div class="photo-container" style="background-image: url('assets/image/habitacion-mojacar-slider1.jpg');">
-                        <div class="discount"><p>75€</p></div>
+                        <div class="discount"><p>Desde 75€</p></div>
                     </div>
                     <div class="title">Habitación Mojácar</div>
                     <div class="description">Muy lejos, más allá de las montañas de palabras, alejados de los países.</div>
