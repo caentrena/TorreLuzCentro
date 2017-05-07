05358689b96d1dc098eb97a69d1e6a021f74e6bb
Isaac Fdez <sakro.developer@gmail.com>
2017-05-07 15:58:23 +0200

    layout of contact page done!

---
+++ contact.php
@@ -0,0 +1,53 @@
+<?php include 'header.php';?>
+
+    <section class="contact-page page-content">
+        <div class="title-page"><p>Contacto</p></div>
+        <div class="form-slider small-width">
+            <div class="form-slider-container">
+                <div class="form-slider-page">
+                    <div class="row-form">
+                        <div class="label required"><p>Nombre *</p></div>
+                        <div class="input"><input class="input-text" type="text"></div>
+                    </div>
+                    <div class="row-form">
+                        <div class="label required"><p>Apellidos *</p></div>
+                        <div class="input"><input class="input-text" type="text"></div>
+                    </div>
+                </div>
+                <div class="form-slider-page">
+                    <div class="row-form">
+                        <div class="label required"><p>Email *</p></div>
+                        <div class="input"><input class="input-text" type="text"></div>
+                    </div>
+                    <div class="row-form">
+                        <div class="label required"><p>Teléfono de contacto</p></div>
+                        <div class="input"><input class="input-text" type="text"></div>
+                    </div>
+                </div>
+                <div class="form-slider-page">
+                    <div class="row-form">
+                        <div class="label required"><p>Mensaje *</p></div>
+                        <div class="input">
+                            <textarea required class="input-text-area"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="conditions-container">
+                <div class="condition-container">
+                    <input required type="checkbox">
+                    <p>Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete.</p>
+                </div>
+                <div class="condition-container">
+                    <input required type="checkbox">
+                    <p>Tablos, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no t</p>
+                </div>
+            </div>
+            <div class="buttons">
+                <div class="next-button button"><p>Siguiente</p></div>
+                <div class="prev-button button"><p>Anterior</p></div>
+            </div>
+        </div>
+    </section>
+    
+<?php include 'footer.php';?>
---
+++ js/main.js
@@ -0,0 +1,84 @@
+var currentFormPage = 0;
+var amountsOfFormPages = $(".contact-page .form-slider .form-slider-container .form-slider-page").length;
+var inTransition = false;
+
+
+$(document).ready(function(){
+	bindEvents();
+	initializePage();
+});
+
+function bindEvents(){
+	$(".contact-page .form-slider .buttons .prev-button").click(changeFormPage);
+	$(".contact-page .form-slider .buttons .next-button").click(changeFormPage);
+}
+
+function initializePage(){
+
+	if($(".contact-page").index() != -1){
+		initializeForm($(".contact-page .form-slider"));
+	}
+	
+}
+
+function initializeForm(form){
+	var pages = form.find(".form-slider-container .form-slider-page");
+	$(pages[0]).css({"display":"block","opacity":"1"});
+}
+
+function changeFormPage(){
+	if(!inTransition){
+		inTrasition = true;
+		if($(this).hasClass("prev-button")){
+			if(currentFormPage == 1){
+				disableButton($(".contact-page .form-slider .buttons .prev-button"));
+			}
+			$(this).parent().find(".next-button").find("p").text("Siguiente")
+			makeAnimationToRightOfForm(currentFormPage - 1);
+		}else if($(this).hasClass("next-button")){
+			enableButton($(".contact-page .form-slider .buttons .prev-button"));
+			//cambiar texto
+			if(currentFormPage >= amountsOfFormPages -2){
+				$(this).find("p").text("Enviar");
+			}
+			//deshabilitar el boton
+			if(currentFormPage < amountsOfFormPages -1){
+				console.log("ir a siguiente");
+				makeAnimationToLeftOfForm(currentFormPage + 1);
+			}else{
+				inTrasition = false;
+			}
+		}
+	}
+}
+
+function makeAnimationToLeftOfForm(indexOfNext){
+	var pages = $(".contact-page .form-slider .form-slider-container .form-slider-page");
+	var tl = new TimelineMax();
+	$(pages[indexOfNext]).css("display","block");
+	tl.to($(pages[indexOfNext]), 0, {x: 20},0)
+	.to( $(pages[currentFormPage]), 0.5, {opacity: 0, x: -20, onComplete: function(){ $(pages[currentFormPage]).css("display","none"); }},0)
+	.to($(pages[indexOfNext]), 0.5, {opacity: 1,x: 0, onComplete: function(){
+		currentFormPage = currentFormPage + 1;
+		inTransition = false;
+	}}, 0.5);
+}
+
+function makeAnimationToRightOfForm(indexOfNext){
+	var pages = $(".contact-page .form-slider .form-slider-container .form-slider-page");
+	var tl = new TimelineMax();
+	$(pages[indexOfNext]).css("display","block");
+	tl.to($(pages[indexOfNext]), 0, {x: -20},0)
+	.to( $(pages[currentFormPage]), 0.5, {opacity: 0, x: 20, onComplete: function(){ $(pages[currentFormPage]).css("display","none"); }},0)
+	.to($(pages[indexOfNext]), 0.5, {opacity: 1,x: 0, onComplete: function(){
+		currentFormPage = currentFormPage -1;
+		inTransition = false;	
+	}}, 0.5);
+}
+
+function enableButton(target){
+	target.css("display","block");
+}
+function disableButton(target){
+	target.css("display","none");
+}
